4a88e4ed805d49a0c610753b52fd02c7eb61e472
Priya Lakshmi T <priyalakshmi02121997@gmail.com>
2022-06-22 12:07:41 +0530

    Adding Routes, Db Connectivity and logic

---
+++ server/controllers/posts.js
@@ -0,0 +1,7 @@
+export const getPosts = (req, res) => {
+    res.send('This works !!');
+}
+
+export const createPost = (req, res) => {
+    res.send('Post creation');
+}
--- server/index.js
+++ server/index.js
@@ -2,3 +2,24 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: "true" }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: "true" }))
+
+app.use(cors());
+
+//  Setup DB connectivity via MongoDB Cloud atlas
+
+const CONNECTION_URL = 'mongodb+srv://tpriya:voguepl12@cluster0.gzl6v.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+// mongoose.set('useFindAndModify', false);
