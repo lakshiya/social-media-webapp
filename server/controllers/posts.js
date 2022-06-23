d3fa1d28e00203dc3d7377613ba6fc1196ec4652
Priya Lakshmi T <priyalakshmi02121997@gmail.com>
2022-06-23 19:49:43 +0530

    Adding controller and implementing functions

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,7 +1,26 @@
-export const getPosts = (req, res) => {
-    res.send('This works !!');
+import postMessage from "../models/postMessage.js";
+
+export const getPosts = async(req, res) => {
+    try {
+        const postMessages = await postMessage.find();
+        console.log(postMessages);
+        res.status(200).json(postMessages);
+    } catch (error) {
+
+        res.status(404).json({ message: error.message });
+
+    }
 }
 
-export const createPost = (req, res) => {
-    res.send('Post creation');
+export const createPost = async(req, res) => {
+    const post_body = req.body();
+
+    const new_post = new postMessage(post_body);
+    try {
+        await new_post.save();
+        res.status(201).json(new_post);
+
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
 }
